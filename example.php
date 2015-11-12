08759179192e3668633f51e4d3a56578374e4a0f
Elminson De Oleo Baez <edeoleo@gmail.com>
2015-11-12 00:01:32 -0500

    Example Query

--- example.php
+++
@@ -1,7 +0,0 @@
-<?php
-$insert="INSERT INTO `npanxx` (`id`, `npa`, `nxx`, `xi`, `xf`, `npanxx`, `region`, `provincia`, `cliente`, `servicio`, `tipored`, `localidad`, `remoto`, `central_host`, `fecha_inst`, `fecha_serv`, `compania`) VALUES (1, '809', '201', '00', '99', '809201', 'No aplica', 'No aplica', '', 'CELULAR PREPAGO', 'Movil', '30 de marzo', '', '30 de Marzo AUTOPLEX', '17/06/2005', '01/07/2005', 'CODETEL');
-";
-$class= new table_from_insert();
-echo $class->create($insert);
-
-?>
